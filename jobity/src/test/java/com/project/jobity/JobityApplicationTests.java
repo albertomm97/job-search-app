b9b9467aa40441d3b57c17fbe7174c48f0d215da
Alberto Marcos <albertomrcs97@gmail.com>
2022-11-29 18:45:03 +0100

    Added initial config for backend application

---
+++ jobity/src/test/java/com/project/jobity/JobityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jobity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
